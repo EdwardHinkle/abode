21151f2a9e240a6fd7682166eee6f3ae38961ae6
Edward Hinkle <eddie@meetmaestro.com>
2017-04-04 20:42:25 -0400

    use git pull instead of git status

--- server-src/git/index.ts
+++ server-src/git/index.ts
@@ -4,7 +4,7 @@ import * as childProcess from 'child_process';
 export function runGitPull(): Promise<any> {
     return new Promise((resolve, reject) => {
     
-        childProcess.exec('cd ' + __dirname + '; cd ../../; git status', (error, stdout, stderr, res) => {
+        childProcess.exec('cd ' + __dirname + '; cd ../../; git pull', (error, stdout, stderr, res) => {
             if (error) {
                 reject(error);
             }
