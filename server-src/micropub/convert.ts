6d98ffaf4893a4161d598e4723dcc736bb5e97e5
Edward Hinkle <eddie@meetmaestro.com>
2017-04-26 20:13:48 -0400

    Make local convert script more flexible

--- server-src/micropub/convert.ts
+++ server-src/micropub/convert.ts
@@ -2,7 +2,8 @@ import * as fs from 'fs';
 import { convertMicropubToJekyll } from './handle';
 
 export function convertSavedJsonToMarkdown(req, res) {
-    fs.readFile(__dirname + "/../../../micropub-examples/1492135312.json", 'utf8', (err, data) => {
+    var filename = req.params.filename;
+    fs.readFile(__dirname + "/../../../micropub-examples/" + filename + ".json", 'utf8', (err, data) => {
         if (err) throw err;
         
         var rehydratedData = JSON.parse(data);
