a5a3b803a0be9c008bae93059ab9fd04f6dfc122
Edward Hinkle <eddie@meetmaestro.com>
2017-05-01 13:53:15 -0400

    stage, commit and rebase pull before building
    site and repushing

--- server-src/process-server/rebuildServer.ts
+++ server-src/process-server/rebuildServer.ts
@@ -5,32 +5,35 @@ import * as jekyll from '../jekyll';
 import * as git from '../git';
 
 export function rebuildServer(req?, res?) {
-    git.runGitPull().then(() => {
+    git.runGitStageAll()
+        .then(() => { return git.runGitCommit(); })
+        .then(() => { return git.runGitPull() })
+        .then(() => {
 
-        return Promise.all([
-            goodreads.getGoodreadsData(),
-            webmentions.getWebmentionData()
-        ]).then((results) => {
-            // All tasks are done, we can restart the jekyll server, etc.
-            console.log("Rebuild ready...");
+            return Promise.all([
+                goodreads.getGoodreadsData(),
+                webmentions.getWebmentionData()
+            ]).then((results) => {
+                // All tasks are done, we can restart the jekyll server, etc.
+                console.log("Rebuild ready...");
 
-            return jekyll.runJekyllBuild()
-            .then(() => { return git.runGitStageAll(); })
-            .then(() => { return git.runGitCommit(); })
-            .then(() => { return git.runGitPush(); })
-            .then(() => {
-                if (res != undefined) {
-                    res.status(202).send('Site Rebuilt');
-                }
-            }).catch((error) => {
-                console.log("Caught Error");
-                console.log(error);
-                if (error != undefined && res != undefined) {
-                    res.status(202).send(error);
-                }
+                return jekyll.runJekyllBuild()
+                .then(() => { return git.runGitStageAll(); })
+                .then(() => { return git.runGitCommit(); })
+                .then(() => { return git.runGitPush(); })
+                .then(() => {
+                    if (res != undefined) {
+                        res.status(202).send('Site Rebuilt');
+                    }
+                }).catch((error) => {
+                    console.log("Caught Error");
+                    console.log(error);
+                    if (error != undefined && res != undefined) {
+                        res.status(202).send(error);
+                    }
+                });
+                
             });
-            
-        });
 
     });
 }
