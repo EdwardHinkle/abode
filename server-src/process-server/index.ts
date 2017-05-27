b98c8cd91413380d7203f10878765b6dcba0c1df
Edward Hinkle <eddie@meetmaestro.com>
2017-05-27 18:16:51 -0400

    Add slack rebuild command

--- server-src/process-server/index.ts
+++ server-src/process-server/index.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import * as path from 'path';
-import { rebuildServer, refreshServer, refreshServerWithDrafts } from './rebuildServer';
+import { rebuildServer, refreshServer, refreshServerWithDrafts, rebuildServerFromSlack } from './rebuildServer';
 
 export let processRouter = express.Router();
 
@@ -8,3 +8,4 @@ export let processRouter = express.Router();
 processRouter.get('/rebuild', rebuildServer);
 processRouter.get('/refresh', refreshServer);
 processRouter.get('/draftRefresh', refreshServerWithDrafts);
+processRouter.post('/rebuild/slack', rebuildServerFromSlack);
--- server-src/process-server/rebuildServer.ts
+++ server-src/process-server/rebuildServer.ts
@@ -4,6 +4,7 @@ import * as webmentions from '../webmentions';
 import * as configTools from '../config';
 import * as jekyll from '../jekyll';
 import * as git from '../git';
+import * as request from 'request';
 
 export function rebuildServer(req?, res?) {
     git.runGitStageAll()
@@ -32,6 +33,94 @@ export function rebuildServer(req?, res?) {
                     console.log(error);
                     if (error != undefined && res != undefined) {
                         res.status(202).send(error);
+                    }
+                });
+                
+            });
+
+    });
+}
+
+export function rebuildServerFromSlack(req?, res?) {
+
+    if (req.body.token != "h0uOKoXJklurbaIY6AbqW9PZ") {
+        console.log("Unauthorized Rebuild Attempt From Slack");
+        res.status(401).send("denied");
+        return;
+    }
+
+    res.status(200).send("Site rebuild process...");
+
+    git.runGitStageAll()
+        .then(() => { return git.runGitCommit(); })
+        .then(() => { return git.runGitPull() })
+        .then(() => {
+
+            return Promise.all([
+                goodreads.getGoodreadsData(),
+                webmentions.getWebmentionData(),
+                configTools.importPeopleData()
+            ]).then((results) => {
+                // All tasks are done, we can restart the jekyll server, etc.
+                console.log("Rebuild ready...");
+                request.post({
+                    url: req.body.response_url,
+                    json: {
+                        "text": "Imports finished, running jekyll"
+                    }
+                }, (err, data) => {
+                    if (err != undefined) {
+                        console.log(`ERROR: ${err}`);
+                    }
+                    if (data.statusCode != 200) {
+                        console.log("oops Slack Error");
+                    } else {
+                        console.log("Successfull sent Slack Message");
+                    }
+
+                });
+
+                return jekyll.runJekyllBuild()
+                .then(() => { return git.runGitStageAll(); })
+                .then(() => { return git.runGitCommit(); })
+                .then(() => { return git.runGitPush(); })
+                .then(() => {
+                    request.post({
+                        url: req.body.response_url,
+                        json: {
+                            "text": "Rebuild Complete"
+                        }
+                    }, (err, data) => {
+                        if (err != undefined) {
+                            console.log(`ERROR: ${err}`);
+                        }
+                        if (data.statusCode != 200) {
+                            console.log("oops Slack Error");
+                        } else {
+                            console.log("Successfull sent Slack Message");
+                        }
+
+                    });
+                }).catch((error) => {
+                    console.log("Caught Error");
+                    console.log(error);
+                    if (error != undefined) {
+                        request.post({
+                            url: req.body.response_url,
+                            json: {
+                                "text": "Uh, oh! There was an error: " + error
+                            }
+                        }, (err, data) => {
+                            if (err != undefined) {
+                                console.log(`ERROR: ${err}`);
+                            }
+                            if (data.statusCode != 200) {
+                                console.log("oops Slack Error");
+                            } else {
+                                console.log("Successfull sent Slack Message");
+                            }
+
+                        });
                     }
                 });
                 
