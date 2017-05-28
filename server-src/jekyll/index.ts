b0435a4a47e0aeb6d11ef5115dcb6979829a4f6f
Edward Hinkle <eddie@meetmaestro.com>
2017-05-27 21:11:06 -0400

    Use jekyll incremental build

--- server-src/jekyll/index.ts
+++ server-src/jekyll/index.ts
@@ -4,7 +4,7 @@ import * as childProcess from 'child_process';
 export function runJekyllBuild(): Promise<any> {
     return new Promise((resolve, reject) => {
     
-        childProcess.exec('cd ' + __dirname + '; cd ../../jekyll/; jekyll build', (error, stdout, stderr, res) => {
+        childProcess.exec('cd ' + __dirname + '; cd ../../jekyll/; jekyll build --incremental', (error, stdout, stderr, res) => {
             if (error) {
                 reject(error);
             }
