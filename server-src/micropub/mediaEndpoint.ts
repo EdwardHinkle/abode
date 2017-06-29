097770ecedf9c8a09ac82e53080b210f8ccc829e
Edward Hinkle <eddie@meetmaestro.com>
2017-06-29 19:56:53 -0400

    Start adding token support to media endpoint

--- server-src/micropub/mediaEndpoint.ts
+++ server-src/micropub/mediaEndpoint.ts
@@ -1,39 +1,51 @@
-import * as MicropubFormatter from 'format-microformat';
-import * as moment from 'moment';
 import * as fs from 'fs';
-import * as path from 'path';
-import * as http from 'http';
-import * as git from '../git';
-import * as jekyll from '../jekyll';
-import * as _ from 'lodash';
-import * as mfTypes from '../mf2';
-import * as yaml from 'js-yaml';
-import * as toMarkdown from 'to-markdown';
-import * as mfo from 'mf-obj';
 import * as mime from 'mime-types';
+import * as request from 'request';
 
 let config = require('../../abodeConfig.json');
 let dataDirPath = __dirname + "/../../jekyll/_source";
 let imageDirPath = `images`;
-let imageDir = `${dataDirPath}/${imageDirPath}`;
 let entryImagePath = `entry-images`;
 
 let imageType = require('image-type');
-let readingTime = require('reading-time');
 
 export function getMediaEndpointRequest(req, res) {
 
     console.log("Reached Endpoint");
 
-    // Get file path with filename
-    let filePath = req.file.path;
-    let fileExt = mime.extension(req.file.mimetype);
-    let fileName = `${req.file.filename}.${fileExt}`;
+    if (req.headers.authorization === undefined || req.headers.authorization.indexOf('Bearer') == -1) {
+        // ERROR no auth bearer token
+        console.log("NO TOKEN");
+    }
 
-    // Rename file with correct extension
-    fs.renameSync(filePath, `${filePath}.${fileExt}`);
+    // Need to check that the access token is valid
+    request.get({
+        'url': config.micropub.tokenEndpoint,
+        'auth': {
+            'bearer': req.headers.authorization.split(" ").pop("Bearer ")
+        }
+    }, (err, data) => {
+        if (err != undefined) {
+            console.log(`ERROR: ${err}`);
+        }
+        console.log("Token Verification Response");
+        console.log(data);
 
-    // Send Response
-    res.location(`${config.server}/${imageDirPath}/${entryImagePath}/${fileName}`).status(201).send("ok");
+        // If false ERROR
+        if (true) {
+            console.log("WRONG TOKEN");
+        }
+
+        // Get file path with filename
+            let filePath = req.file.path;
+        let fileExt = mime.extension(req.file.mimetype);
+        let fileName = `${req.file.filename}.${fileExt}`;
+
+        // Rename file with correct extension
+        fs.renameSync(filePath, `${filePath}.${fileExt}`);
+
+        // Send Response
+        res.location(`${config.server}/${imageDirPath}/${entryImagePath}/${fileName}`).status(201).send("ok");
+    });
 
 }
