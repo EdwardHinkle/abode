fdf2ef12292357a0cc9e34f399b79aaefc8a374c
Eddie Hinkle <eddie@eddiehinkle.com>
2018-01-21 15:51:38 -0500

    Remove static site auth logging

--- server-src/static-site/index.ts
+++ server-src/static-site/index.ts
@@ -5,13 +5,9 @@ export let staticSiteRouter = express.Router();
 
 // Routes
 let PublicPrivateSite = (req, res, next) => {
-    console.log("Logged in?");
-    console.log(req.session.username);
     if (req.session.username === undefined) {
-        console.log("Public build");
         express.static(path.join(__dirname, '../../jekyll/_build/'))(req, res, next);
     } else {
-        console.log("Private build");
         express.static(path.join(__dirname, '../../jekyll/_private_build/'))(req, res, next);
     }
 };
