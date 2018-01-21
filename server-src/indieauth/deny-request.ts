eaef2dc07a4d410bd03f4b89566a260b514fc5ae
Eddie Hinkle <eddie@eddiehinkle.com>
2018-01-21 14:42:27 -0500

    Do some logging on the deny request function

--- server-src/indieauth/deny-request.ts
+++ server-src/indieauth/deny-request.ts
@@ -8,15 +8,20 @@ import * as request from 'request';
 
 export let denyRequest = (req, res, next) => {
 
-    if (req.session.indieAuthRequest === undefined || req.session.indieAuthRequest.redirect_uri === undefined) {
+    let requestInfo = req.session.indieAuth;
+
+    console.log("request session");
+    console.log(requestInfo);
+
+    if (requestInfo.indieAuthRequest === undefined || requestInfo.indieAuthRequest.redirect_uri === undefined) {
         console.log('Either no IndieAuth Request or no Redirect URI');
         next()
     }
 
-    let requestInfo = req.session.indieAuth;
+
     let redirectUrl = requestInfo.redirect_uri + (requestInfo.redirect_uri.indexOf('?') > -1 ? '?' : '&') + 'error=access_denied';
 
-    delete req.session.indieAuth;
     res.redirect(redirectUrl);
 
+    delete req.session.indieAuth;
 };
