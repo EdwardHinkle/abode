b193ac057eb6e67185c3c079bf05b1fe2868f50d
Eddie Hinkle <eddie@eddiehinkle.com>
2018-08-13 13:48:15 -0400

    Tweak activity pub profile

--- server-src/activity-pub/index.ts
+++ server-src/activity-pub/index.ts
@@ -5,42 +5,48 @@ export let activityPubRouter = express.Router();
 activityPubRouter.get('/.well-known/webfinger', (req, res) => {
     let config: any = req.app.get('config');
 
-    res.json({
-        "subject": "acct:me@eddiehinkle.com",
+    if (req.accepts('json')) {
+        res.json({
+            "subject": "acct:me@eddiehinkle.com",
 
-        "links": [
-            {
-                "rel": "self",
-                "type": "application/activity+json",
-                "href": "https://eddiehinkle.com/activitypub/actor"
-            }
-        ]
-    });
+            "links": [
+                {
+                    "rel": "self",
+                    "type": "application/activity+json",
+                    "href": "https://eddiehinkle.com/activitypub"
+                }
+            ]
+        });
+    }
 });
 
-activityPubRouter.get('/activitypub/actor', (req, res) => {
+activityPubRouter.get('/activitypub', (req, res) => {
     let config: any = req.app.get('config');
 
-    res.json({
-        "@context": [
-            "https://www.w3.org/ns/activitystreams",
-            "https://w3id.org/security/v1"
-        ],
+    if (req.accepts('json')) {
+        res.json({
+            "@context": [
+                "https://www.w3.org/ns/activitystreams",
+                "https://w3id.org/security/v1"
+            ],
 
-        "id": "https://eddiehinkle.com/activitypub/actor",
-        "type": "Person",
-        "preferredUsername": "eddiehinkle",
-        "inbox": "https://eddiehinkle.com/activitypub/inbox",
-        "name": "Eddie Hinkle",
-        "summary": "Eddie Hinkle is a husband to Ashley, father to Luke and UI Engineer that lives in Frederick, MD.",
-        "icon": [
-            "https://eddiehinkle.com/images/profile.jpg"
-        ],
+            "id": "https://eddiehinkle.com/activitypub",
+            "type": "Person",
+            "preferredUsername": "eddiehinkle",
+            "inbox": "https://eddiehinkle.com/activitypub/inbox",
+            "name": "Eddie Hinkle",
+            "summary": "Eddie Hinkle is a husband to Ashley, father to Luke and UI Engineer that lives in Frederick, MD.",
+            "icon": {
+                "type": "Image",
+                "mediaType": "image/jpeg",
+                "url": "https://eddiehinkle.com/images/profile.jpg"
+            },
 
-        "publicKey": {
-            "id": "https://eddiehinkle.com/activitypub/actor#main-key",
-            "owner": "https://eddiehinkle.com/activitypub/actor",
-            "publicKeyPem": config.activitypub.pem
-        }
-    });
+            "publicKey": {
+                "id": "https://eddiehinkle.com/activitypub#main-key",
+                "owner": "https://eddiehinkle.com/activitypub",
+                "publicKeyPem": config.activitypub.pem
+            }
+        });
+    }
 });
