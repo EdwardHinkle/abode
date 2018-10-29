87eec8c65c2171a73fcf67187f14ccd69501d382
Eddie Hinkle <eddie@eddiehinkle.com>
2018-10-29 12:19:54 -0400

    Fix up location ping endpoint

--- server-src/location/index.ts
+++ server-src/location/index.ts
@@ -3,9 +3,10 @@ import * as express from "express";
 export let locationRouter = express.Router();
 
 // Routes
-locationRouter.get('/ping', locationPing);
+locationRouter.post('/ping', locationPing);
 
 function locationPing(req, res, next) {
     console.log('Location Ping');
     console.log(req.body);
+    res.status(200).send('location updated');
 }
